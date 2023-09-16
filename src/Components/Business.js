709376ecdcf2214ab0be827f80c79614fd3e2cc7
edgarb <edgar_daniel_1996@hotmail.com>
2023-09-16 13:11:25 -0600

    Add business component boilerplate

---
+++ src/Components/Business.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Business() {
+    return (
+        <div className='c-business'>
+            <img src='' alt='Business image'/>
+            <p>Name</p>
+            <p>Address</p>
+            <p>City</p>
+            <p>State</p>
+            <p>Zip code</p>
+            <p>Category</p>
+            <p>Rating</p>
+            <p>Count</p>
+        </div>
+    );
+}
