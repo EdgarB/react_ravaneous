fb8f098ba6e4b6d72d7d96758a3bc6768c5c2e5c
edgarb <edgar_daniel_1996@hotmail.com>
2023-09-16 16:17:56 -0600

    Update business component

--- src/Components/Business.js
+++ src/Components/Business.js
@@ -4,15 +4,20 @@ import './Business.scss';
 function Business(props) {
     return (
         <div className='c-business'>
-            <img src={props.image} alt='Business image'/>
-            <p>{props.name}</p>
-            <p>{props.address}</p>
-            <p>{props.city}</p>
-            <p>{props.state}</p>
-            <p>{props.zipCode}</p>
-            <p>{props.category}</p>
-            <p>{props.rating} stars</p>
-            <p>{props.reviewCount} reviews</p>
+          <img src={props.image} alt='Business image'/>
+          <p className='c-business__title'>{props.name}</p>
+          <div className='c-business__columns'>
+            <div className='c-business__first-col'>
+              <p>{props.address}</p>
+              <p>{props.city}</p>
+              <p className='c-business__row'>{props.state} {props.zipCode}</p>
+            </div>
+            <div className='c-business__second-col'>
+              <p className='c-business__category'>{props.category}</p>
+              <p className='c-business__rating'>{props.rating} stars</p>
+              <p>{props.reviewCount} reviews</p>
+            </div>
+          </div>
         </div>
     );
 }
