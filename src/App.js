f7497e0b463677c3e5de95dca76a37950fe5eccc
edgarb <edgar_daniel_1996@hotmail.com>
2023-09-16 14:20:52 -0600

    Move business list logic to BusinessList component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import './App.scss';
-import Business from './Components/Business';
+import BusinessList from './Components/BusinessList';
 
-function restaurantFactory(){
-  return (
-    {
-      name: 'The restaurant',
-      image: 'https://dummyimage.com/200x200/000/fff.jpg',
-      address: 'Random address',
-      city: 'New York',
-      state: 'New York',
-      zip_code: '10001',
-      category: 'Elegante',
-      rating: '5',
-      review_count: '42'
-    }
-  );
-}
 
-const dummyRestaurants = [
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory(),
-  restaurantFactory()
-]
 
 function App() {
   return (
@@ -37,22 +9,7 @@ function App() {
       <h1>Welcome to Ravenous!</h1>
       <p>Here you'll find the best restaurants recomendations depending on your area!</p>
       
-      <div className='c-app__restaurants'>
-        {dummyRestaurants.map( r => {
-          return (
-            <Business 
-              name={r.name}
-              address={r.address}
-              image= {r.image}
-              city= {r.city}
-              state= {r.state}
-              zipCode= {r.zip_code}
-              category= {r.category}
-              rating= {r.rating}
-              reviewCount= {r.review_count}
-            />)
-        })}
-      </div>
+      <BusinessList/> 
     </div>
   );
 }
---
+++ src/Components/BusinessList.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Business from './Business';
+import './BusinessList.scss';
+
+function restaurantFactory(){
+    return (
+      {
+        name: 'The restaurant',
+        image: 'https://dummyimage.com/200x200/000/fff.jpg',
+        address: 'Random address',
+        city: 'New York',
+        state: 'New York',
+        zip_code: '10001',
+        category: 'Elegante',
+        rating: '5',
+        review_count: '42'
+      }
+    );
+  }
+  
+  const dummyRestaurants = [
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory(),
+    restaurantFactory()
+  ]
+
+function BusinessList(){
+    return(
+      <div className='c-business-list'>
+        {dummyRestaurants.map( r => {
+          return (
+            <Business 
+              name={r.name}
+              address={r.address}
+              image= {r.image}
+              city= {r.city}
+              state= {r.state}
+              zipCode= {r.zip_code}
+              category= {r.category}
+              rating= {r.rating}
+              reviewCount= {r.review_count}
+            />)
+          })}
+      </div>
+    );
+}
+
+export default BusinessList;
