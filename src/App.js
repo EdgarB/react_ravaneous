e64b748b17ef47f1de7c9ffc6977b5c3c17559b6
edgarb <edgar_daniel_1996@hotmail.com>
2023-09-18 14:02:47 -0600

    update image credit style

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ function App() {
     <div className="c-app">
       <div className='c-app__header'>
         <img src={headerImg} className='c-app__header-img'/>
+        <div className='c-app__header-img-credit'>
+          Foto de <a href="https://unsplash.com/es/@nadineprimeau?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nadine Primeau</a> en <a href="https://unsplash.com/es/fotos/-bLkT8wGV0I?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </div>
         <div className='c-app__header-content'>
           <h1 className='c-app__title'>Welcome to Ravenous!</h1>
           <p className='c-app__description'>Here you'll find the best restaurants recomendations depending on your area!</p>
           <SearchBar/>
-          Foto de <a href="https://unsplash.com/es/@nadineprimeau?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nadine Primeau</a> en <a href="https://unsplash.com/es/fotos/-bLkT8wGV0I?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
         </div>
       </div>
       <div className='c-app__body'>
