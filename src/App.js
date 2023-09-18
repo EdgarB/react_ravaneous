d4b5e750181c1cce6599f74fa3cd846dceccf8b5
edgarb <edgar_daniel_1996@hotmail.com>
2023-09-18 14:23:33 -0600

    Update app general style and add NavBar component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import './App.scss';
 import BusinessList from './Components/BusinessList';
 import SearchBar from './Components/SearchBar';
 import headerImg from './header.jpg';
-
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="c-app">
+      <NavBar/>
       <div className='c-app__header'>
         <img src={headerImg} className='c-app__header-img'/>
         <div className='c-app__header-img-credit'>
           Foto de <a href="https://unsplash.com/es/@nadineprimeau?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nadine Primeau</a> en <a href="https://unsplash.com/es/fotos/-bLkT8wGV0I?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
         </div>
         <div className='c-app__header-content'>
-          <h1 className='c-app__title'>Welcome to Ravenous!</h1>
-          <p className='c-app__description'>Here you'll find the best restaurants recomendations depending on your area!</p>
           <SearchBar/>
         </div>
       </div>
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.scss';
+
+function NavBar(){
+  return(
+    <div className='c-nav-bar'>
+      <div className='c-nav-bar__brand'>
+        ravenous
+      </div>
+    </div>
+  )
+}
+
+export default NavBar;
